acfe8718e4c6e65d7daf3c62e2828d23354ca3a4
erfan <ehoseyni1383@gmail.com>
2024-03-13 22:53:22 +0330

    update

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
@@ -12,6 +14,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
 
   bool ontap = false;
+  bool ontap2 = false;
+
+
+  Timer? _timer;
+  int _start = 0;
+
+  void startTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_start < 1) {
+          timer.cancel();
+        } else {
+          _start--;
+          if(_start == 0){
+            ontap = false;
+          }
+        }
+      });
+    });
+  }
+
+  String formatTime(int seconds) {
+    int hours = seconds ~/ 3600;
+    seconds %= 3600;
+    int minutes = seconds ~/ 60;
+    seconds %= 60;
+    return '${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+  }
 
 
   @override
@@ -46,9 +79,16 @@ class _HomeState extends State<Home> {
                             duration: const Duration(milliseconds: 100)
                         ),
 
-
+                          Text(
+                            formatTime(_start),
+                            style:
+                              const TextStyle(color: Colors.white,fontSize: 40)
+                          ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     SizedBox(
-                      width: Get.width/2,
+
                       height: 55,
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
@@ -60,14 +100,42 @@ class _HomeState extends State<Home> {
                         onPressed: () {
                           setState(() {
                             ontap = true;
+                            _start += 5;
                           });
+                          startTimer();
                         }, child: const Text(
                           "add Time for connection",
                         style: TextStyle(color: Colors.black),
                       ),
 
                       ),
-                    )
+                    ),
+                    if(ontap)
+                    SizedBox(
+                      height: 70,
+                      child: InputChip(
+                              side: const BorderSide(color: Colors.transparent),
+                             padding: const EdgeInsets.fromLTRB(40,15,40,15),
+                             label: Text(ontap2 == false ? 'Connect' : 'Disconnect'),
+                             labelStyle: const TextStyle(color: Colors.white),
+
+                             backgroundColor: ontap ? Colors.green : Colors.grey,
+
+                             onSelected: _start > 0 ? (bool value) {
+                               setState(() {
+                                 if(ontap == true) {
+                                   ontap2 = value;
+                                 } else {
+                                   null;
+                                 }
+                               });
+                             }:null,
+                             selected: ontap2,
+                             showCheckmark: false,
+                             selectedColor: ontap2 == true ? Colors.red : Colors.green,
+                           ),
+                    ),
+
                         ])
                 )
             )
